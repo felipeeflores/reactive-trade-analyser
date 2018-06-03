4dcfea9486838e32292bef17dc1ca42dba136263
Felipe Flores <felipe.flores@rea-group.com>
2018-06-04 03:30:30 +1000

    Limit the output to the specified value.
    Part of #7

--- src/main/scala/analyser/ConsoleOps.scala
+++ src/main/scala/analyser/ConsoleOps.scala
@@ -13,12 +13,12 @@ object ConsoleOps {
       |> ./analyser $SYMBOLS [[optional] $DAYS] [[optional] IND=$INDICATORS]
       |
       |where:
-      |\t $SYMBOLS
-      |\t\t List of ASX ticker codes separated by comma (i.e. AEE, AGO, PLS)
-      |\t $DAYS
-      |\t\t Size of the data frame in days; it is provided as an integer
-      |\t $INDICATORS
-      |\t\t List of supported indicators that user want to see included in the final report (i.e. 10SMA, 50SMA, 10EMA)
+      | $SYMBOLS
+      |   List of ASX ticker codes separated by comma (i.e. AEE, AGO, PLS)
+      | $DAYS
+      |   Size of the data frame in days; it is provided as an integer
+      | $INDICATORS
+      |   List of supported indicators that user want to see included in the final report (i.e. 10SMA, 50SMA, 10EMA)
     """.stripMargin
 
   def printError(error: String) = Console.println(s"${RESET}${RED}$error${RESET}")
--- src/main/scala/analyser/Reporter.scala
+++ src/main/scala/analyser/Reporter.scala
@@ -9,13 +9,14 @@ object Reporter {
   def reportResult(results: Vector[EnrichedStockMarketEntry], limit: Option[Int], indicatorTypes: Vector[IndicatorType]): Task[Unit] = {
     Task {
       printOK("SYM,DATE,EOD," + indicatorTypes.map(_.toString).mkString(","))
-      results.foreach(result => printOK(
-        f"${result.stockMarketEntry.ticker.value},${result.stockMarketEntry.date.value},${result.stockMarketEntry.close.value}%.3f," +
-        indicatorTypes.map(indicatorType =>
-          f"${result.indicators.find(_.indicatorType == indicatorType).map(_.indicatorValue.value).getOrElse(0.0)}%.3f"
-        ).mkString(",")
-
-      ))
+      results.slice(0, limit.getOrElse(Int.MaxValue)).foreach(result =>
+        printOK(
+          f"${result.stockMarketEntry.ticker.value},${result.stockMarketEntry.date.value},${result.stockMarketEntry.close.value}%.3f," +
+          indicatorTypes.map(indicatorType =>
+            f"${result.indicators.find(_.indicatorType == indicatorType).map(_.indicatorValue.value).getOrElse(0.0)}%.3f"
+          ).mkString(",")
+        )
+      )
     }
   }
 }
